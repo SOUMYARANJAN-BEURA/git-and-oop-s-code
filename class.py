367aaf94471080faece4bad9a498675664bd44af
Soumyaranjan-Beura <soumyaranjanbeura99@gmail.com>
2024-11-13 21:23:38 +0530

    oop's class code and (innitial commit)

---
+++ class.py
@@ -0,0 +1,24 @@
+class persion():
+    def address (self,name,age):
+    #def __person_address__(self,name,age):
+       self.name = name
+       self.age = age
+
+
+persion1 = ("soumya","25")
+persion2 = ("Hari","23")
+persion3 = ("Ram","20")
+persion4 = ("Gopal","22")
+persion5 = ("Shyam",24)
+
+name='OOOOOOOOOO'
+age=  1111111111
+
+obj1=('abcd',123)
+obj2=(name,age)
+
+print(persion1,persion2,persion5,persion4,persion3, sep = " @ ")
+print(persion1,persion2,persion5,persion4,persion3, sep = " \n ")
+
+print(obj1, end='correct')
+print('\n',obj2)
