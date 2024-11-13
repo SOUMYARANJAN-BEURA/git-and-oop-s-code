bccf6bb7bcd038713648bc05dea696ebe310e985
Soumyaranjan-Beura <soumyaranjanbeura99@gmail.com>
2024-11-13 21:29:28 +0530

    oop's iterator first commit.

---
+++ Iterator.py
@@ -0,0 +1,18 @@
+a = [1,2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8,9]
+b = iter(a)
+
+
+
+'''print(next(b))
+print(next(b))
+print(next(b))
+print(next(b))
+print(next(b))
+print(next(b))
+print(next(b))'''
+
+
+
+
+for b in a:
+    print(b)
